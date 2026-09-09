4d225871ff10aa3195bc69ecceb75bf5dc5679b7
jem-Asus <jeremychuahtm@gmail.com>
2026-09-09 10:01:47 +0800

    fix(hitbox): switched to hitbox edge checking instead of corner to handle certain cases when hitboxes overlap but no corners are within the other hitbox

--- SRCS/Hyperbolics/Hitbox.cpp
+++ SRCS/Hyperbolics/Hitbox.cpp
@@ -1,6 +1,7 @@
 #include "Hitbox.hpp"
 #include <iostream>
 #include <cmath>
+#include <array>
 #include "MinkowskiCoord.hpp"
 #include "KleinCoord.hpp"
 
@@ -22,29 +23,85 @@ bool	isPointInRect(const KleinCoord& topLeft, const KleinCoord& botRight, const 
 	return false;
 }
 
-bool	cornerCollides(const Hitbox& hitbox, const MinkowskiCoord& pos, const Hitbox& other, const MinkowskiCoord& otherPos)
+Vector3	crossProduct(const MinkowskiCoord& a, const MinkowskiCoord& b)
 {
-	KleinCoord	kleinTopLeft(-std::tanh(hitbox.width() / 2), std::tanh(hitbox.height() / 2));
-	KleinCoord	kleinBotRight(-kleinTopLeft.x(), -kleinTopLeft.y());
+	return {
+		a.y() * b.z() - a.z() * b.y(),
+		a.z() * b.x() - a.x() * b.z(),
+		a.x() * b.y() - a.y() * b.x()
+	};
+}
 
-	KleinCoord	otherKleinCorner(-std::tanh(other.width() / 2), -std::tanh(other.height() / 2));
-	MinkowskiCoord	otherMinkowskiCorner1 = otherKleinCorner.toMinkowski();
-	MinkowskiCoord	otherMinkowskiCorner2 = KleinCoord(-otherKleinCorner.x(), otherKleinCorner.y()).toMinkowski();
-	MinkowskiCoord	otherMinkowskiCorner3 = KleinCoord(otherKleinCorner.x(), -otherKleinCorner.y()).toMinkowski();
-	MinkowskiCoord	otherMinkowskiCorner4 = KleinCoord(-otherKleinCorner.x(), -otherKleinCorner.y()).toMinkowski();
+Vector3	crossProduct(const Vector3& a, const Vector3& b)
+{
+	return {
+		a.y * b.z - a.z * b.y,
+		a.z * b.x - a.x * b.z,
+		a.x * b.y - a.y * b.x
+	};
+}
+
+float	innerProduct(const MinkowskiCoord& a, const MinkowskiCoord& b)
+{
+	return a.z() * b.z() - a.y() * b.y() - a.x() * b.x();
+}
+
+bool	geodesicIntersects(const MinkowskiCoord& p1, const MinkowskiCoord& p2, const MinkowskiCoord& q1, const MinkowskiCoord q2)
+{
+	Vector3	crossProductP = crossProduct(p1, p2);
+	Vector3 crossProductQ = crossProduct(q1, q2);
+	Vector3	pxq = crossProduct(crossProductP, crossProductQ);
+	float	pxqInnerProduct = pxq.z * pxq.z - pxq.y * pxq.y - pxq.x * pxq.x;
+
+	if (pxqInnerProduct <= 0)
+		return false;
 	
-	if (isPointInRect(kleinTopLeft, kleinBotRight, otherMinkowskiCorner1.inverseRelativeTo(otherPos).relativeTo(pos).toKlein())
-		|| isPointInRect(kleinTopLeft, kleinBotRight, otherMinkowskiCorner2.inverseRelativeTo(otherPos).relativeTo(pos).toKlein())
-		|| isPointInRect(kleinTopLeft, kleinBotRight, otherMinkowskiCorner3.inverseRelativeTo(otherPos).relativeTo(pos).toKlein())
-		|| isPointInRect(kleinTopLeft, kleinBotRight, otherMinkowskiCorner4.inverseRelativeTo(otherPos).relativeTo(pos).toKlein())
-	)
+	float	pxqRootInnerProduct = std::sqrt(pxqInnerProduct);
+	if (pxq.z < 0)
+		pxqRootInnerProduct *= -1;
+	MinkowskiCoord intersection(pxq.x / pxqRootInnerProduct, pxq.y / pxqRootInnerProduct);
+
+	if (innerProduct(intersection, p1) < innerProduct(p1, p2) && innerProduct(intersection, p2) < innerProduct(p1, p2)
+		&& (innerProduct(intersection, q1) < innerProduct(q1, q2) && innerProduct(intersection, q2) < innerProduct(q1, q2)))
 		return true;
 	return false;
 }
 
 bool	Hitbox::collides(const MinkowskiCoord& pos, const Hitbox& other, const MinkowskiCoord& otherPos) const
 {
-	if (cornerCollides(*this, pos, other, otherPos) || cornerCollides(other, otherPos, *this, pos))
+	KleinCoord	kleinTopRight(std::tanh(width() / 2), std::tanh(height() / 2));
+
+	KleinCoord	otherKleinCorner(std::tanh(other.width() / 2), std::tanh(other.height() / 2));
+	MinkowskiCoord	otherMinkowskiCorner1 = otherKleinCorner.toMinkowski();
+	MinkowskiCoord	otherMinkowskiCorner2 = KleinCoord(-otherKleinCorner.x(), otherKleinCorner.y()).toMinkowski();
+	MinkowskiCoord	otherMinkowskiCorner3 = KleinCoord(-otherKleinCorner.x(), -otherKleinCorner.y()).toMinkowski();
+	MinkowskiCoord	otherMinkowskiCorner4 = KleinCoord(otherKleinCorner.x(), -otherKleinCorner.y()).toMinkowski();
+
+	std::array<MinkowskiCoord, 4>	corners = {
+		kleinTopRight.toMinkowski(),
+		KleinCoord(-kleinTopRight.x(), kleinTopRight.y()).toMinkowski(),
+		KleinCoord(-kleinTopRight.x(), -kleinTopRight.y()).toMinkowski(),
+		KleinCoord(kleinTopRight.x(), -kleinTopRight.y()).toMinkowski()
+	};
+	
+	std::array<MinkowskiCoord, 4>	otherCorners = {
+		otherMinkowskiCorner1.inverseRelativeTo(otherPos).relativeTo(pos),
+		otherMinkowskiCorner2.inverseRelativeTo(otherPos).relativeTo(pos),
+		otherMinkowskiCorner3.inverseRelativeTo(otherPos).relativeTo(pos),
+		otherMinkowskiCorner4.inverseRelativeTo(otherPos).relativeTo(pos)
+	
+	};
+
+	for (size_t i = 0; i < 4; ++i)
+	{
+		for (size_t j = 0; j < 4; ++j)
+		{
+			if (geodesicIntersects(corners[i], corners[(i + 1) % 4], otherCorners[j], otherCorners[(j + 1) % 4]))
+				return true;
+		}
+	}
+	if (isPointInRect(corners[1].toKlein(), corners[3].toKlein(), otherCorners[0].toKlein())
+		|| isPointInRect(otherCorners[1].toKlein(), otherCorners[3].toKlein(), corners[0].toKlein()))
 		return true;
 	return false;
 }
