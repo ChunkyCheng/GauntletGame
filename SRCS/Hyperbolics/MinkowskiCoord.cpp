5175dc556ec08db58fddcaa9e2941185b6ac014a
jem-Asus <jeremychuahtm@gmail.com>
2026-09-09 10:31:36 +0800

    refactor: moved minkowski's xyz into a vector3 struct

--- SRCS/Hyperbolics/MinkowskiCoord.cpp
+++ SRCS/Hyperbolics/MinkowskiCoord.cpp
@@ -5,24 +5,22 @@
 #include "KleinCoord.hpp"
 
 MinkowskiCoord::MinkowskiCoord(float x, float y)
-	: m_x(x)
-	, m_y(y)
-	, m_z(std::sqrt(x * x + y * y + 1))
+	: m_vector({x, y, std::sqrt(x * x + y * y + 1)})
 {}
 
 PoincareCoord	MinkowskiCoord::toPoincare(void) const
 {
-	return PoincareCoord(m_x / (m_z + 1), m_y / (m_z + 1));
+	return PoincareCoord(x() / (z() + 1), y() / (z() + 1));
 }
 
 KleinCoord	MinkowskiCoord::toKlein(void) const
 {
-	return KleinCoord(m_x / m_z, m_y / m_z);
+	return KleinCoord(x() / z(), y() / z());
 }
 
 float	MinkowskiCoord::hDist(const MinkowskiCoord& other) const
 {
-	return std::acosh(m_z * other.m_z - m_x * other.m_x - m_y * other.m_y);
+	return std::acosh(z() * other.z() - x() * other.x() - y() * other.y());
 }
 
 //Finds the first 2 rows of a matrix X such that X(other) = (0, 0, 1)
@@ -33,10 +31,10 @@ MinkowskiCoord	MinkowskiCoord::relativeTo(const MinkowskiCoord& other) const
 	Vector3	rowX = other.relativeRowX();
 	Vector3	rowY = other.relativeRowY();
 
-	float	x = rowX.x * m_x + rowX.y * m_y + rowX.z * m_z;
-	float	y = rowY.x * m_x + rowY.y * m_y + rowY.z * m_z;
+	float	newX = rowX.x * x() + rowX.y * y() + rowX.z * z();
+	float	newY = rowY.x * x() + rowY.y * y() + rowY.z * z();
 
-	return MinkowskiCoord(x, y);
+	return MinkowskiCoord(newX, newY);
 }
 
 MinkowskiCoord	MinkowskiCoord::inverseRelativeTo(const MinkowskiCoord& other) const
@@ -44,21 +42,21 @@ MinkowskiCoord	MinkowskiCoord::inverseRelativeTo(const MinkowskiCoord& other) co
 	Vector3	rowX = other.invRelativeRowX();
 	Vector3	rowY = other.invRelativeRowY();
 
-	float	x = rowX.x * m_x + rowX.y * m_y + rowX.z * m_z;
-	float	y = rowY.x * m_x + rowY.y * m_y + rowY.z * m_z;
-	return MinkowskiCoord(x, y);
+	float	newX = rowX.x * x() + rowX.y * y() + rowX.z * z();
+	float	newY = rowY.x * x() + rowY.y * y() + rowY.z * z();
+	return MinkowskiCoord(newX, newY);
 }
 
 void	MinkowskiCoord::moveXHyperbolic(float dist)
 {
-	m_x = std::cosh(dist) * m_x + std::sinh(dist) * m_z;
-	m_z = std::sqrt(m_x * m_x + m_y * m_y + 1);
+	m_vector.x = std::cosh(dist) * x() + std::sinh(dist) * z();
+	m_vector.z = std::sqrt(x() * x() + y() * y() + 1);
 }
 
 void	MinkowskiCoord::moveYHyperbolic(float dist)
 {
-	m_y = std::cosh(dist) * m_y + std::sinh(dist) * m_z;
-	m_z = std::sqrt(m_x * m_x + m_y * m_y + 1);
+	m_vector.y = std::cosh(dist) * y() + std::sinh(dist) * z();
+	m_vector.z = std::sqrt(x() * x() + y() * y() + 1);
 }
 
 void	MinkowskiCoord::moveHyperbolic(float dist, float theta)
@@ -68,57 +66,58 @@ void	MinkowskiCoord::moveHyperbolic(float dist, float theta)
 	Vector3	invRowX = invRelativeRowX();
 	Vector3 invRowY = invRelativeRowY();
 
-	m_x = RQ[0] * invRowX.x + RQ[1] * invRowX.y + RQ[2] * invRowX.z;
-	m_y = RQ[0] * invRowY.x + RQ[1] * invRowY.y + RQ[2] * invRowY.z;
-	m_z = std::sqrt(m_x * m_x + m_y * m_y + 1);
+	m_vector.x = RQ[0] * invRowX.x + RQ[1] * invRowX.y + RQ[2] * invRowX.z;
+	m_vector.y = RQ[0] * invRowY.x + RQ[1] * invRowY.y + RQ[2] * invRowY.z;
+	m_vector.z = std::sqrt(x() * x() + y() * y() + 1);
 }
 
 Vector3	MinkowskiCoord::relativeRowX(void) const
 {
 	return {
-		1 + m_x * m_x / (m_z + 1),
-		m_x * m_y / (m_z + 1),
-		-m_x
+		1 + x() * x() / (z() + 1),
+		x() * y() / (z() + 1),
+		-x()
 	};	
 }
 
 Vector3	MinkowskiCoord::relativeRowY(void) const
 {
 	return {
-		m_x * m_y / (m_z + 1),
-		1 + m_y * m_y / (m_z + 1),
-		-m_y
+		x() * y() / (z() + 1),
+		1 + y() * y() / (z() + 1),
+		-y()
 	};
 }
 
 Vector3	MinkowskiCoord::invRelativeRowX(void) const
 {
 	return {
-		1 + m_x * m_x / (m_z + 1),
-		m_x * m_y / (m_z + 1),
-		m_x
+		1 + x() * x() / (z() + 1),
+		x() * y() / (z() + 1),
+		x()
 	};	
 }
 
 Vector3	MinkowskiCoord::invRelativeRowY(void) const
 {
 	return {
-		m_x * m_y / (m_z + 1),
-		1 + m_y * m_y / (m_z + 1),
-		m_y
+		x() * y() / (z() + 1),
+		1 + y() * y() / (z() + 1),
+		y()
 	};
 }
 
 void	MinkowskiCoord::setXY(float x, float y)
 {
-	m_x = x;
-	m_y = y;
-	m_z = std::sqrt(x * x + y * y + 1);
+	m_vector.x = x;
+	m_vector.y = y;
+	m_vector.z = std::sqrt(x * x + y * y + 1);
 }
 
-float	MinkowskiCoord::x(void) const { return m_x; }
-float	MinkowskiCoord::y(void) const { return m_y; }
-float	MinkowskiCoord::z(void) const { return m_z; }
+float			MinkowskiCoord::x(void) const { return m_vector.x; }
+float			MinkowskiCoord::y(void) const { return m_vector.y; }
+float			MinkowskiCoord::z(void) const { return m_vector.z; }
+const Vector3&	MinkowskiCoord::vector(void) const { return m_vector; }
 
 std::ostream&	operator<<(std::ostream& os, const MinkowskiCoord& m)
 {
--- SRCS/Hyperbolics/MinkowskiCoord.hpp
+++ SRCS/Hyperbolics/MinkowskiCoord.hpp
@@ -24,18 +24,17 @@ class	MinkowskiCoord
 		Vector3			invRelativeRowX(void) const;
 		Vector3			invRelativeRowY(void) const;
 
-		void	moveXHyperbolic(float dist);
-		void	moveYHyperbolic(float dist);
-		void	moveHyperbolic(float dist, float theta);
-		void	setXY(float x, float y);
-		float	x(void) const;
-		float	y(void) const;
-		float	z(void) const;
+		void			moveXHyperbolic(float dist);
+		void			moveYHyperbolic(float dist);
+		void			moveHyperbolic(float dist, float theta);
+		void			setXY(float x, float y);
+		float			x(void) const;
+		float			y(void) const;
+		float			z(void) const;
+		const Vector3&	vector(void) const;
 
 	private:
-		float	m_x;
-		float	m_y;
-		float	m_z;
+		Vector3	m_vector;
 };
 
 std::ostream&	operator<<(std::ostream& os, const MinkowskiCoord& m);
